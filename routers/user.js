f8ddd24e5de5e1a76cef9d07db3209e42aa7d665
leubenia <hdodo777@gmail.com>
2021-10-25 10:55:18 +0900

    test

--- routers/user.js
+++ routers/user.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const controller = require('../controller/user')
 // const { emailCheck, login, sigup , nickNameCheck } = require('../controller/user')
 
-router.route('/login').post(controller.login,function(req, res){});
+router.route('/login').post(controller.login);
 router.route('/sinup').post(controller.signup);
-router.route('/checkemil').post(controller.emailCheck,function(req, res){});
+router.route('/checkemil').post(controller.emailCheck);
 router.route('/checknickname').post(controller.nickNameCheck,function(req, res){});
+
 
 
 module.exports = router;
